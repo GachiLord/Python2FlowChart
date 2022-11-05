db77f1aeab2c28a3814ac4a278948e2a06d9133c
oleg <name504172@gmail.com>
2022-11-05 18:33:12 +0500

    removed useless files

--- Python2FlowChart/PyChart/__init__.py
+++
@@ -1,8 +0,0 @@
-try:
-    from PyChart.BlockDiagram import *
-    from PyChart.Preprocessor import *
-    from PyChart.PseudoCode import *
-except ModuleNotFoundError:
-    from BlockDiagram import *
-    from Preprocessor import *
-    from PseudoCode import *
